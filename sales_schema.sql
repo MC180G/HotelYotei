6b7da4c33488233fc9b5ffbb9825d44eb2135fb6
Matt Colson <mc180g@gmail.com>
2024-10-21 18:02:32 -0400

    Update sales_schema.sql
    Diagram and code now match; Diagram to replace place holder image

--- sales_schema.sql
+++ sales_schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE room
 (
 id INT AUTO_INCREMENT NOT NULL ,
 room_number INT,
+  -- What is catagory? Like suites, single twin bed pods, etc?
 catagory_id INT,
 price MONEY,
 flr_num INT,
@@ -37,10 +38,10 @@ CREATE TABLE staff
 id INT AUTO_Increment NOT NULL,
 first_name VARCHAR,
 last_name VARCHAR,
+position_id INT,
 dept_id INT,
 shift TINYINT,
 -- 1=6a/2p, 2=2p/10p, 3=10p/6a
-position_id INT,
 );
 
 CREATE TABLE booking
@@ -48,17 +49,22 @@ CREATE TABLE booking
 id INT AUTO_INCREMENT NOT NULL,
 room_id INT,
 guest_id INT,
+staff_id INT,
 available BOOLEAN,
+  -- in this case "available" means both not occupied AND clean; there may be rare cases where the guest changes during a stay or some deal is made, and the BOOLEAN will say it was NOT available when booked
 date_booked DATETIME,
 check_in DATETIME,
 check_out DATETIME,
 staff_id INT,
 );
 
+-- Food & Beverage; Also refered to as MINI BAR in diagram
 CREATE TABLE fb 
 (
 id INT AUTO_INCREMENT NOT NULL,
 room_id INT,
+guest_id INT,
+  -- in groups, guests may purchase something from another guests mini bar, or room, or treat another guest to a meal or drink
 product_id INT,
 quanity INT,
 price MONEY,
